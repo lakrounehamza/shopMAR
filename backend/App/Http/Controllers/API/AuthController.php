742efc35c053fc9f4b5304b7972c97a84d993428
lakrounehamza <lakrounehamza10@gmail.com>
2025-06-25 14:57:36 +0100

    feat: implement login and registration methods in AuthController

--- backend/App/Http/Controllers/API/AuthController.php
+++ backend/App/Http/Controllers/API/AuthController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Repositories\AuthRepository;
 
 class AuthController extends Controller
 {
+    private AuthRepository $authRepository;
 
+    public function __construct(AuthRepository $authRepository)
+    {
+        $this->authRepository = $authRepository;
+    }
+
+    public function login(LoginRequest $request)
+    {
+        return $this->authRepository->login($request);
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        return $this->authRepository->register($request);
+    }
 }
